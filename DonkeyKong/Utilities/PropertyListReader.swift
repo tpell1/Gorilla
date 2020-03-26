007b80065d2aa0d10574d581231f69c99d8ee626
Travis Pell <travispell@Traviss-Air.lan>
2020-03-26 13:58:25 +0000

    Can now read config in from config.plist

--- DonkeyKong/Utilities/PropertyListReader.swift
+++ DonkeyKong/Utilities/PropertyListReader.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 // =============== Data structures =======================================
-
+//////////////////////// Levels ////////////////////////////
 // Root node, loads in an array of levels
 struct LevelsStruct: Codable {
     var levels: [LevelStruct]
@@ -37,6 +37,13 @@ struct EnemyStruct: Codable {
     var enemyY: Int
 }
 
+///////////////////// Config //////////////////////////
+struct ConfigStruct: Codable {
+    var currentLevel: Int
+    var currentScore: Int
+    var currentLives: Int
+}
+
 // ================ Class implementation =================================
 class PropertyListReader {
     static func readFile(fileName: String) -> String {
@@ -46,6 +53,14 @@ class PropertyListReader {
     static func readLevelFile(fileName: String) -> LevelsStruct? {
         if let file = Bundle.main.path(forResource: fileName, ofType: "plist"), let plist = FileManager.default.contents(atPath: file), let levels = try? PropertyListDecoder().decode(LevelsStruct.self, from: plist) {
             return levels
+        } else {
+            return nil
+        }
+    }
+    
+    static func readConfigFile(fileName: String) -> ConfigStruct? {
+        if let file = Bundle.main.path(forResource: fileName, ofType: "plist"), let plist = FileManager.default.contents(atPath: file), let config = try? PropertyListDecoder().decode(ConfigStruct.self, from: plist) {
+            return config
         } else {
             return nil
         }
